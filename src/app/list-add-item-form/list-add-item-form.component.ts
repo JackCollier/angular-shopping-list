86445ec023c05fbd5a7991f19a979d9cf921de3a
JacvaSipt <steamcow12@hotmail.com>
2023-08-16 11:57:52 +0100

    import shopping list service and create new form control

--- src/app/list-add-item-form/list-add-item-form.component.ts
+++ src/app/list-add-item-form/list-add-item-form.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-list-add-item-form',
   templateUrl: './list-add-item-form.component.html',
-  styleUrls: ['./list-add-item-form.component.css']
+  styleUrls: ['./list-add-item-form.component.css'],
 })
 export class ListAddItemFormComponent {
+  constructor(private shoppingListService: ShoppingListService) {}
 
+  listItemForm = new FormControl({
+    itemName: new FormControl(''),
+  });
 }
