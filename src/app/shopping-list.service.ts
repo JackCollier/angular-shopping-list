c6a7922ba13b21c31823be1d5c6364cd18a75f62
JacvaSipt <steamcow12@hotmail.com>
2023-08-16 11:11:32 +0100

    create methods to retrieve all items from the to buy and previously bought list

--- src/app/shopping-list.service.ts
+++ src/app/shopping-list.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { ListItem } from './list-item';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingListService {
-  constructor() {}
+  private apiUrlToBuyList = 'http://localhost:3000/to-buy-list';
+  private apiUrlPreviouslyBoughtList =
+    'http://localhost:3000/previously-bought-list';
+
+  constructor(private http: HttpClient) {}
+
+  getAllToBuyItems(): Observable<ListItem[]> {
+    return this.http.get<ListItem[]>(this.apiUrlToBuyList);
+  }
+
+  getAllPreviouslyBoughtItems(): Observable<ListItem[]> {
+    return this.http.get<ListItem[]>(this.apiUrlPreviouslyBoughtList);
+  }
 }
