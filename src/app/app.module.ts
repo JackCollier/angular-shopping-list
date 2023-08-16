f7640e4fbebf383a7ab07298e97a2ced1c5e62e9
JacvaSipt <steamcow12@hotmail.com>
2023-08-16 11:54:32 +0100

    import form modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShoppingListDisplayComponent } from './shopping-list-display/shopping-list-display.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ListAddItemFormComponent } from './list-add-item-form/list-add-item-form.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ShoppingListDisplayComponent, ListAddItemFormComponent],
+  declarations: [
+    AppComponent,
+    ShoppingListDisplayComponent,
+    ListAddItemFormComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     DragDropModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
