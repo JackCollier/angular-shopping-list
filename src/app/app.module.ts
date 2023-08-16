d76031dfb00680dfb67623b28cde1aaff5bbfb5a
JacvaSipt <steamcow12@hotmail.com>
2023-08-16 11:41:56 +0100

    import drag drop module from cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShoppingListDisplayComponent } from './shopping-list-display/shopping-list-display.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent, ShoppingListDisplayComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    DragDropModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
